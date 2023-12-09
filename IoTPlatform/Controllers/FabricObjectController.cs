504bd63539d80b13d5a781dca1882624d94d3cd0
cawadead <keiserao@gmail.com>
2023-12-09 21:18:02 +0600

    Дополнен функционал контроллера FabricObject

--- IoTPlatform/Controllers/FabricObjectController.cs
+++ IoTPlatform/Controllers/FabricObjectController.cs
@@ -1,27 +1,58 @@
 using IoTPlatform.Models.Database;
 using IoTPlatform.Services;
 using Microsoft.AspNetCore.Mvc;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace IoTPlatform.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class FabricObjectController : ControllerBase
     {
         private readonly MongoDBService _service;
-        
+
         public FabricObjectController(MongoDBService service)
         {
             _service = service;
         }
 
         [HttpGet]
+        [Route("[controller]/")]
         public async Task<List<FabricObject>> GetAllFabricObjects()
         {
             return await _service.GetAllFabricObjects();
         }
-        
+
+        [HttpGet]
+        [Route("[controller]/{id}")]
+        public async Task<List<FabricObject>> GetFabricObjectById(
+            [Required] string id)
+        {
+            return await _service.GetFabricObjectById(id);
+        }
+
+        [HttpGet]
+        [Route("[controller]/name={name}")]
+        public async Task<List<FabricObject>> GetFabricObjects(
+            [Required] string name)
+        {
+            return await _service.GetFabricObjects(name);
+        }
+
+        [HttpGet]
+        [Route("[controller]/type={type}")]
+        public async Task<List<FabricObject>> GetFabricObjects(
+            [Required] int type)
+        {
+            return await _service.GetFabricObjects(type);
+        }
+
+        [HttpGet]
+        [Route("[controller]/parentId={parentId}")]
+        public async Task<List<FabricObject>> GetFabricObjectsByParentId(
+            [Required] string parentId)
+        {
+            return await _service.GetFabricObjectsByParentId(parentId);
+        }
     }
 }
 
--- IoTPlatform/Services/MongoDBService.cs
+++ IoTPlatform/Services/MongoDBService.cs
@@ -3,6 +3,7 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using IoTPlatform.Models.Database;
 using IoTPlatform.Support;
+using System.Xml.Linq;
 
 namespace IoTPlatform.Services
 {
@@ -18,30 +19,63 @@ namespace IoTPlatform.Services
             _fabricObjectsCollection = database.GetCollection<FabricObject>(mongoDBSettings.Value.FabricObjectsCollectionName);
             _timeSeriesCollection = database.GetCollection<TimeSeries>(mongoDBSettings.Value.TimeSeriesCollectionName);
         }
-
+        #region FabricObjects
         public async Task<List<FabricObject>> GetAllFabricObjects()
         {
             return await _fabricObjectsCollection.Find(new BsonDocument()).ToListAsync();
         }
 
-        public async Task<List<TimeSeries>> GetTimeSeries(DateTime from, DateTime to)
+        public async Task<List<FabricObject>> GetFabricObjects(int type)
+        {
+            var filter = new BsonDocument { { "type", type } };
+            return await _fabricObjectsCollection.Find(filter).ToListAsync();
+        }
+
+        public async Task<List<FabricObject>> GetFabricObjects(string name)
+        {
+            var filter = new BsonDocument { { "name", name } };
+            return await _fabricObjectsCollection.Find(filter).ToListAsync();
+        }
+
+        public async Task<List<FabricObject>> GetFabricObjectsByParentId(string parentId)
+        {
+            var filter = new BsonDocument { { "parentId", parentId } };
+            return await _fabricObjectsCollection.Find(filter).ToListAsync();
+        }
+
+        public async Task<List<FabricObject>> GetFabricObjectById(string id)
+        {
+            var filter = new BsonDocument("_id", ObjectId.Parse(id));
+            return await _fabricObjectsCollection.Find(filter).ToListAsync();
+        }
+        #endregion
+
+        #region TimeSeries
+        public async Task<List<TimeSeries>> GetTimeSeries(
+            DateTime from, 
+            DateTime to)
         {
             return _timeSeriesCollection.AsQueryable().Where(x => x.date >= from && x.date < to).ToList();
         }
-        public async Task<List<List<TimeSeries>>> GetTimeSeriesPaginated(DateTime from, DateTime to, int pageSize)
+        public async Task<List<List<TimeSeries>>> GetTimeSeriesPaginated(
+            DateTime from, 
+            DateTime to, 
+            int pageSize)
         {
             var data = _timeSeriesCollection.AsQueryable().Where(x => x.date >= from && x.date < to).ToList();
             var result = new PaginatedList<TimeSeries>(data, pageSize);
             return result.GetList();
         }
 
-        public async Task<List<TimeSeries>> GetTimeSeriesPage(DateTime from, DateTime to, int pageSize, int pageNumber)
+        public async Task<List<TimeSeries>> GetTimeSeriesPage(
+            DateTime from, 
+            DateTime to, 
+            int pageSize, 
+            int pageNumber)
         {
             var data = _timeSeriesCollection.AsQueryable().Where(x => x.date >= from && x.date < to).ToList();
             return PaginatedList<TimeSeries>.GetListPage(data, pageSize, pageNumber);
         }
-
-
-
+        #endregion
     }
 }

6f38a1f7f2af5133433ace2392cd1a67844be35c
cawadead <keiserao@gmail.com>
2023-12-14 21:29:49 +0600

    добавлен проект с тестами

---
+++ IoTPlatform.Test/Classes/PaginatedListTest.cs
@@ -0,0 +1,27 @@
+namespace IoTPlatform.Test.Classes
+{
+    public class PaginatedListTest
+    {
+        [Fact]
+        public async Task GetListPage()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task GetListPageByPageNumber()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+            Assert.True(true);
+        }
+    }
+}

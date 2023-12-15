708bc23afb3fa9d8a3f83a2c2149e16c67be1479
cawadead <keiserao@gmail.com>
2023-12-15 20:44:43 +0600

    убрана работа через ef core с mongodb

---
+++ IoTPlatform/Context/PostgreContext.cs
@@ -0,0 +1,19 @@
+﻿using IoTPlatform.Models.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace IoTPlatform.Context
+{
+    public class PostgreContext : DbContext
+    {
+        public PostgreContext(DbContextOptions<PostgreContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            base.OnConfiguring(options);
+        }
+
+    }
+}

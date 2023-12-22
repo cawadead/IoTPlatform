08a122a07a12b32cca2378cd05a35489b4ee2605
cawadead <keiserao@gmail.com>
2023-12-22 22:39:11 +0600

    добавлены методы расширения

--- IoTPlatform/Extensions/StringExtension.cs
+++ IoTPlatform/Extensions/StringExtension.cs
@@ -1,16 +1,60 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace IoTPlatform.Extensions
 {
     public static class StringExtension
     {
-        public static byte[] ToByteArray(this string value) { 
-            return Encoding.UTF8.GetBytes(value);
+        /// <summary>
+        /// Convert string to byte array
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static byte[] ToByteArray(this string str) { 
+            return Encoding.UTF8.GetBytes(str);
         }
 
-        public static MemoryStream ToMemoryStream(this string value)
+        /// <summary>
+        /// Convert string to stream
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static MemoryStream ToMemoryStream(this string str)
         {
-            return new MemoryStream(value.ToByteArray());
+            return new MemoryStream(str.ToByteArray());
+        }
+
+        /// <summary>
+        /// Get string hash
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static byte[] GetHash(this string str)
+        {
+            using(HashAlgorithm algorithm = SHA256.Create())
+            {
+                return algorithm.ComputeHash(str.ToByteArray());
+            }
+        }
+
+        /// <summary>
+        /// Get string hash as string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string GetHashString(this string str)
+        {
+            return BitConverter.ToString(str.GetHash());
+        }
+
+        /// <summary>
+        /// Get string hash code as integer
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int GetHashCode(this string str)
+        {
+            return BitConverter.ToInt32(str.GetHash(), 0);
         }
     }
 }
